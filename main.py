05492b129c473e71f830bba290e98625f0b294cc
ACER <210220@softwarica.edu.np>
2021-09-17 15:25:01 +0545

    Check Button Applied in login

--- main.py
+++ main.py
@@ -20,14 +20,29 @@ bg = PhotoImage(file="Images/Loginbg.png")
 background = Label(game, image=bg).place(x=0, y=0)
 
 # adding user login bg
-login_bg = PhotoImage(file="Images/LoginLabelwithterms.png")
+login_bg = PhotoImage(file="Images/LoginLabelwithTerms.png")
 loginframe = Label(game, image=login_bg, bd=0).place(x=761, y=49)
+
+userEntry=Entry(game,text="Enter Username",
+      font=("Arial,30"),
+      bg="#000000",
+      bd=0)
+userEntry.place(x=860,y=102)
 
 # Adding Username Label
 userLabel_img = PhotoImage(file="Images/Userlabel.png")
 userLabel = Label(game, image=userLabel_img, bg="#000000").place(x=790.5, y=60)
 
 # Adding terms and condition check box and text
+checkB=StringVar()
+Checkbutton=Checkbutton(game,variable=checkB,
+                        onvalue="ON",
+                        offvalue="OFF",
+                        bg="#000000",
+                        activebackground="#000000",
+                        bd=0,)
+Checkbutton.deselect()
+Checkbutton.place(x=802,y=143.38)
 
 
 
