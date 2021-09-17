14b692b3b18dd2db43c559f7d5241101659c817d
ACER <210220@softwarica.edu.np>
2021-09-17 12:42:55 +0545

    checkbox

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # from PIL import ImageTk, Image
 
 game = Tk()
@@ -8,18 +9,23 @@ game.geometry("1280x720")
 game.resizable(False, False)
 
 # Adding login backgrounds
-bg=PhotoImage(file="Images/Loginbg.png")
-background = Label(game,image=bg).place(x=0, y=0)
+bg = PhotoImage(file="Images/Loginbg.png")
+background = Label(game, image=bg).place(x=0, y=0)
 
 # adding user login bg
-login_bg=PhotoImage(file="Images/LoginFrameBG.png")
-loginframe=Label(game,image=login_bg,bd=0).place(x=761,y=49)
+login_bg = PhotoImage(file="Images/LoginFrameBG.png")
+loginframe = Label(game, image=login_bg, bd=0).place(x=761, y=49)
 
-#Adding Username Label
-userLabel_img=PhotoImage(file="Images/Userlabel.png")
-userLabel=Label(game,image=userLabel_img,bg="#000000").place(x=767.5,y=79.5)
+# Adding Username Label
+userLabel_img = PhotoImage(file="Images/Userlabel.png")
+userLabel = Label(game, image=userLabel_img, bg="#000000").place(x=790.5, y=79.5)
 
+# Adding terms and condition check box and text
 
+Label(game, text="I accept and understand all terms and conditions ",
+            font=("Arial,8"),
+            bd=0,
+            bg="#FFFFFF").place(x=815, y=150)
 
 """
 def start():
