dfec58ac1fc4ddf6689f8de38151802037a68bf6
ACER <210220@softwarica.edu.np>
2021-10-02 23:45:13 +0545

    signup entry

--- Signup.py
+++ Signup.py
@@ -1,11 +1,18 @@
 from tkinter import*
 game=Tk()
+
+game.geometry("800x600")
+game.resizable(False,False)
 global signupbg
 signup = LabelFrame(game).pack()
 
-# adding signupbg
+# Adding signupbg
+
 signupbg = PhotoImage(file="Images/rolexf1.png")
-signupbackg = Label(signup, image=signupbg, relief=FLAT, bd=0).place(x=0, y=0)
+signupbackg = Label(signup, image=signupbg, relief=FLAT, bd=0).place(x=-110, y=-100)
+
+signup_blackframe=PhotoImage(file="Images/")
+sup_blackframe=Label(signup,image=signup_blackframe,relief=FLAT,bd=0).place(x=,y=)
 
 
 
