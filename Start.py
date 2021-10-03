214d8d84271961ac70253c31d89ccd158f8c58c0
ACER <210220@softwarica.edu.np>
2021-10-04 01:24:25 +0545

    dashboard

--- Start.py
+++ Start.py
@@ -1,16 +1,25 @@
 from tkinter import *
-import sqlite3
+
 root = Tk()
+root.geometry("1280x720")
+root.resizable(False, False)
 
 
+def dashboard():
+    global startButton_img
+    start_game = LabelFrame(root).place(x=0, y=0)
 
+    startbg_img = PhotoImage(file="Images/f1.png")
+    startbg = Label(root, image=startbg_img).place(x=0, y=0)
 
+    startButton_img = PhotoImage(file="Images/StartButton.png")
+    startButton = Button(root, image=startButton_img,
+                         bg="#000000",fg="#000000",
+                         activebackground="#000000",
+                         relief=FLAT,
+                         bd=0,
+                         )
+    startButton.place(x=600, y=500)
 
-
-
-
-
-
-
-
-mainloop()
+dashboard()
+root.mainloop()
